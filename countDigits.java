8bf64d0ab3ff1725326c68a7b2379410e5c038b6
ANUBHAV SRIVASTAVA <anubhavsrivastav12398@gmail.com>
2025-04-05 16:01:41 +0530

    Create countDigits.java

---
+++ countDigits.java
@@ -0,0 +1,12 @@
+import java.util.*;
+	class countDigits {
+		public static void main(String[] args) {
+		        Scanner sc=new Scanner(System.in);
+		        System.out.println("ENTER A NUMBER:");
+		        int x=sc.nextInt();
+		        int count=0;
+		        for(;x>0;x/=10)
+		           count++;
+		        System.out.println("NUMBER OF DIGITS IN THE GIVEN NUMBER IS "+count);		
+	}
+}
