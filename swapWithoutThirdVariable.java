3bafb8c612ed63b6c2ec168db283674b34c3753d
ANUBHAV SRIVASTAVA <anubhavsrivastav12398@gmail.com>
2025-04-05 16:43:09 +0530

    Update swapWithoutThirdVariable.java

--- swapWithoutThirdVariable.java
+++ swapWithoutThirdVariable.java
@@ -1,5 +1,5 @@
 import java.util.*;
-	class swapWithoutThirdVariable {
+	class SwapWithoutThirdVariable {
 		public static void main(String[] args) {
 		        Scanner sc=new Scanner(System.in);
 		        System.out.println("ENTER 1ST NUMBER:");
