22873d9a965681b056a8b57ce7ceb4d0910223b8
ANUBHAV SRIVASTAVA <anubhavsrivastav12398@gmail.com>
2025-04-05 15:18:37 +0530

    Create addWithoutThirdVariable.java

---
+++ addWithoutThirdVariable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+	class addWithoutThirdVariable {
+		public static void main(String[] args) {
+		        Scanner sc=new Scanner(System.in);
+		        System.out.println("ENTER 1ST NUMBER:");
+		        int x=sc.nextInt();
+		        System.out.println("ENTER 2ND NUMBER:");
+		        int y=sc.nextInt();
+		        x+=y;
+		        System.out.println("SUM OF THE GIVEN 2 NUMBERS= "+x);		
+	}
+}
