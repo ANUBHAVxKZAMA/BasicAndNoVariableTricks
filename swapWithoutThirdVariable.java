ea4ff45609f90dc2562e94566a31858a4a706037
ANUBHAV SRIVASTAVA <anubhavsrivastav12398@gmail.com>
2025-04-05 15:25:09 +0530

    Create swapWithoutThirdVariable.java

---
+++ swapWithoutThirdVariable.java
@@ -0,0 +1,14 @@
+import java.util.*;
+	class swapWithoutThirdVariable {
+		public static void main(String[] args) {
+		        Scanner sc=new Scanner(System.in);
+		        System.out.println("ENTER 1ST NUMBER:");
+		        int x=sc.nextInt();
+		        System.out.println("ENTER 2ND NUMBER:");
+		        int y=sc.nextInt();
+		        x+=y;
+		        y=x-y;
+		        x-=y;
+		        System.out.println("AFTER SWAPPING THE NUMBERS ARE "+x+" AND " +y);		
+	}
+}
