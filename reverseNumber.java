b261769de2337272c03d82bd1856f0d81c12b253
ANUBHAV SRIVASTAVA <anubhavsrivastav12398@gmail.com>
2025-04-05 15:57:08 +0530

    Create reverseNumber.java

---
+++ reverseNumber.java
@@ -0,0 +1,14 @@
+mport java.util.*;
+	class reverseNumber {
+		public static void main(String[] args) {
+		        Scanner sc=new Scanner(System.in);
+		        System.out.println("ENTER A NUMBER:");
+		        int x=sc.nextInt();
+		        int rev=0,lastdig;
+		        for(;x>0;x/=10){
+		            lastdig=x%10;
+		            rev=rev*10+lastdig;
+		        }
+		        System.out.println("AFTER REVERSING THE NUMBER IS "+rev);		
+	}
+}
